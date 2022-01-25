1e2414229c35bbb665bf03185896242cf83a781d
ChaehwanLee <oasisinyourmind@gmail.com>
2022-01-25 17:49:46 +0900

    Changes 연산자 조건 수정

--- basic/OprTest.py
+++ basic/OprTest.py
@@ -16,4 +16,17 @@ print(5<=3)
 print(5!=3) #같지 않다=다르다
 
 #논리연산자
-num1
+num1 = 100
+num2 = 50
+
+#And 연산 모든 조건이 True면 True를 Return
+if num1 >= 90 and num2 >= 90:
+    print("통과")
+else:
+    print("다시작업")
+
+#Or 연산 모든 조건 중 한 개 이상 True면 True를 Return
+if num1 >= 90 or num2 >= 90:
+    print("통과")
+else:
+    print("다시작업")
