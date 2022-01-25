59a2841642d9dd7b2cb35e7631050f7c5e72c84a
ChaehwanLee <oasisinyourmind@gmail.com>
2022-01-25 17:42:39 +0900

    Changes 나이성별 input 연산자 추가

---
+++ basic/OprTest.py
@@ -0,0 +1,19 @@
+# 산술연산자
+print(2+3)
+print(2-3)
+print(2*3) #곱하기
+print(2**3) #제곱
+print(11/2) #나누기(소숫점 이하도 출력)
+print(11%2) #나머지구하기
+print(11//2) #나누기(소숫점 이하 버리기)
+
+#비교연산자(True나 False를 결과로 Return)
+print(5>3)
+print(5<3)
+print(5==3) #같다
+print(5>=3)
+print(5<=3)
+print(5!=3) #같지 않다=다르다
+
+#논리연산자
+num1
