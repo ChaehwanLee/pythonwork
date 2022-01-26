c3593557337ce8f70489308f7d674203f6c0df86
ChaehwanLee <oasisinyourmind@gmail.com>
2022-01-26 10:20:28 +0900

    Changes %d f c s 에 관한 내용까지

--- basic/inoutput_test.py
+++ basic/inoutput_test.py
@@ -6,3 +6,10 @@ num2 = int(input("숫자2:"))
 # 표준출력
 print("입력숫자1:", num1) #문자열과 변수 num1의 입력된 값을 콘솔(출력창)로 출력하기
 print("입력숫자2:", num2)
+
+print(num1,num2)
+print(num1,num2, sep=',')
+
+print("%d / %d = %d" % (100, 5, 20)) # %d 정수가 온다
+print("%d / %d = %d" % (num1, num2, num1/num2))
+print("%d / %d = %f" % (num1, num2, num1/num2))
--- statement/ifTest2.py
+++ statement/ifTest2.py
@@ -11,6 +11,6 @@ if jumsu>=60: #변수가 60점 이상
     if examcout<=3: # 위의 조건을 만족하는 데이터중 examcount가 3이하 데이터는 합격처리
         print("합격")
     else: # 점수가 60점 이상이지만 examcount 가 3보 크면 불합격으로 처리
-        print("불합격")
+        print("불합격1")
 else:
-    print("불합격")
+    print("불합격2")
