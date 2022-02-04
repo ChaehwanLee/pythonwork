e02d96245308d99be0d206193d9126f6805dc02e
ChaehwanLee <oasisinyourmind@gmail.com>
2022-02-04 11:48:26 +0900

    Changes 2.4 count

--- String/stringExam04.py
+++ String/stringExam04.py
@@ -2,12 +2,33 @@
 # - 1부터 10000까지의 숫자에서 8이라는 숫자가 총 몇 번 나오는지 체크하기
 # - 8이 포함되어 있는 숫자의 갯수를 카운팅하는 것이 아니라 8이라는 숫자가 몇 번 나오는지 카운트
 # - 예를 들어 8888은 4로 카운팅, 1288은 2로 카운팅
-# - for 문을 중첩해서 사용
-# 정답 : 4000
+# - for 문을 중첩해서 사용 : 로직으로 구현하기
+# - 로직으로 구현하는 것이 끝나면 함수로 구현해보기 : count 활용
+# - 정답 : 4000
 
-for i in range(1:10001):
-    for i in range
-    if i == '8':
-        print("8의 갯수", i)
-    else:
-        print()
+# data = 1000
+# for i in str(data):
+#     print(i, type(i))
+# range 는 숫자라서 안되겠구나
+
+# for i in range(1,10001):
+#     print("i=", i)
+#     for k in str(i):
+#         print('K=',k)
+#         if k == "8":
+#             print(k,"8이다")
+
+count = 0 # 8의 횟수를 저장할 변수 #숫자는 반복작업을 못해서 숫자를 문자열로 변환해서 한 문자씩 접근 후 비교작업 수행
+for i in range(1,10001):
+    for k in str(i):
+        if k == "8":
+            count = count + 1
+
+print("8의 횟수=", count)
+
+print("="*50)
+mycount = 0
+for i in range(1, 10001):
+    mycount = mycount + str(i).count("8")
+print("8의 횟수=", count)
+
